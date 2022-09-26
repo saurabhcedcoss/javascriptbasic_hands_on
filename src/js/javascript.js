eec0e359c99ff018f05644004e2b37df7f4b5c8f
saurabhcedcoss <saurabhtripathi@cedcoss.com>
2022-09-26 11:48:58 +0530

    Create a variable called x, assign the value 50 to it.

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,2 +1,4 @@
-//Exercise 1.Create a variable called carName, assign the value Volvo to it.
+//Exercise 1. Create a variable called carName, assign the value Volvo to it.
 var carName = "Volvo";
+//Exercise 2. Create a variable called x, assign the value 50 to it.
+var x = 50;
