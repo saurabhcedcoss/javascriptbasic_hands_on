d16aed77c2f2ef58f7ad89944da67dd66daa70ab
saurabhcedcoss <saurabhtripathi@cedcoss.com>
2022-09-26 11:51:14 +0530

    Exercise 3. Display the sum of 5 + 10, using two variables: x and y.

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -2,3 +2,7 @@
 var carName = "Volvo";
 //Exercise 2. Create a variable called x, assign the value 50 to it.
 var x = 50;
+//Exercise 3. Display the sum of 5 + 10, using two variables: x and y.
+var a = 1;
+var b = 10;
+document.getElementById("demo").innerHTML = a + b;
